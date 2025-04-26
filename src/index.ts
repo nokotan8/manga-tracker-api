886dc6b664455c01cfdefc393d793751a3969cbe
Justin Tao <z5476270@ad.unsw.edu.au>
2025-04-26 21:01:23 +1000

    error checking for login route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
 import auth from '#routes/auth.js';
 
 const app = express();
 const port = '9292';
 
 app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.originalUrl}`);
+    console.log(
+        `[${new Date().toLocaleString()}] ${req.method} ${req.originalUrl} ${res.statusCode}`,
+    );
     next();
 });
 
 app.use('/auth', auth);
+
+app.use((req, res) => {
+    // Unmatched route
+    res.status(404).json({ errors: [`Route ${req.url} does not exist`]})
+})
+
+app.use(((err, req, res, next) => {
+    console.log(err.message);
+    if (err.status === 400) {
+        return res.status(400).json({ errors: ['Invalid JSON'] })
+    }
+
+    res.status(500).json({ message: 'Internal server error' });
+}) as ErrorRequestHandler);
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,29 @@
+// /auth
 import express, { Request, Response, Router } from 'express';
+import { body, Result, validationResult } from 'express-validator';
 
 const router: Router = express.Router();
 
-// POST /auth/register
+router.post(
+    '/login',
+    body('username')
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Username must be between 4 and 20 characters'),
+    body('password')
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 and 20 characters'),
+    (req: Request, res: Response) => {
+        const result: Result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.status(400).json({
+                errors: result.formatWith((error) => error.msg as string).array(),
+            });
+            return;
+        }
+        res.json({ token: 'jwt here' });
+    },
+);
+
 router.post('/register', (req: Request, res: Response) => {
     try {
         res.json({ token: 'jwt here' });
