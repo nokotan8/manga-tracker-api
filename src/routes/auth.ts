b70b78a1261462eafee03a9dc40bff362a8390f3
fancywhale23 <justintaoau@gmail.com>
2025-04-27 20:24:16 +1000

    replaced mysql with mysql2

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, Router } from "express";
+import mysql
 import { body, Result, validationResult } from "express-validator";
 
 const router: Router = express.Router();
