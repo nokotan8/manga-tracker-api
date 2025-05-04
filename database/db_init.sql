67214d10462d261cb994523d10bb5f0d31911daf
fancywhale23 <justintaoau@gmail.com>
2025-05-04 14:53:27 +1000

    added more not null constraints to db schema

--- database/db_init.sql
+++ database/db_init.sql
@@ -2,23 +2,22 @@ CREATE TABLE IF NOT EXISTS Users (
     Id Varchar(32),
     Username VARCHAR(20) NOT NULL UNIQUE,
     Password VARCHAR(255) NOT NULL,
-    Salt CHAR(16) NOT NULL,
 
     PRIMARY KEY (Id)
 );
 
 CREATE TABLE IF NOT EXISTS Manga (
     Id VARCHAR(32),
-    TitleEN TEXT NOT NULL,
-    TitleJP TEXT,
+    TitleEN TEXT,
+    TitleJP TEXT NOT NULL,
     AuthorEN TEXT,
     AuthorJP TEXT,
     Year SMALLINT,
-    Owner VARCHAR(32),
+    Owner VARCHAR(32) NOT NULL,
     Chapters INT,
     Volumes INT,
-    PublicationStatus TEXT,
-    IsPublic BOOLEAN,
+    PublicationStatus TEXT NOT NULL,
+    IsPublic BOOLEAN NOT NULL,
 
     PRIMARY KEY (Id),
     FOREIGN KEY (Owner) REFERENCES Users (Id)
@@ -26,8 +25,8 @@ CREATE TABLE IF NOT EXISTS Manga (
 
 CREATE TABLE IF NOT EXISTS Genres (
     Id INT AUTO_INCREMENT,
-    Genre TEXT,
-    MangaId VARCHAR(32),
+    Genre TEXT NOT NULL,
+    MangaId VARCHAR(32) NOT NULL,
 
     PRIMARY KEY (Id),
     FOREIGN KEY (MangaId) REFERENCES Manga (Id)
@@ -35,32 +34,31 @@ CREATE TABLE IF NOT EXISTS Genres (
 
 CREATE TABLE IF NOT EXISTS MangaLists (
     Id VARCHAR(32),
-    Name TEXT,
-    Owner Varchar(32),
+    Name TEXT NOT NULL,
+    Owner Varchar(32) NOT NULL,
 
     PRIMARY KEY (Id)
 );
 
 CREATE TABLE IF NOT EXISTS ListEntry (
     Id VARCHAR(32),
-    IsManga VARCHAR(32),
+    IsManga VARCHAR(32) NOT NULL,
     ChaptersRead INT,
     VolumesRead INT,
-    ReadStatus TEXT,
+    ReadStatus TEXT NOT NULL,
     Score SMALLINT,
     Notes TEXT,
 
     PRIMARY KEY (Id),
-    FOREIGN KEY (InList) REFERENCES MangaLists (Id),
     FOREIGN KEY (IsManga) REFERENCES Manga (Id)
 );
 
 CREATE TABLE IF NOT EXISTS InList (
     Id INT AUTO_INCREMENT,
-    ListEntry VARCHAR(32),
-    InList VARCHAR(32),
+    ListEntry VARCHAR(32) NOT NULL,
+    EntryIn VARCHAR(32) NOT NULL,
 
     PRIMARY KEY (Id),
     FOREIGN KEY (ListEntry) REFERENCES ListEntry (Id),
-    FOREIGN KEY (InList) REFERENCES MangaLists (Id)
+    FOREIGN KEY (EntryIn) REFERENCES MangaLists (Id)
 );
